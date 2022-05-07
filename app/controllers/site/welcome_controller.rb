76becb2a36c3968e9f7549fb0958d26acf5ef4f6
muellerviana <muellerviana@gmail.com>
2022-05-07 00:35:24 -0300

    aula 130, criado site controller e namespace

---
+++ app/controllers/site/welcome_controller.rb
@@ -0,0 +1,6 @@
+class Site::WelcomeController < SiteController
+
+  layout 'site'
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :admins_profiles do
     get 'welcome/index'
   end
@@ -7,8 +10,7 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
-
-  root 'welcome#index'
+  
+  root 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
