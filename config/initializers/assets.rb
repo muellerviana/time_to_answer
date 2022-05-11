8aa991e145e5102199e4e28b4a0992a53a1b2873
muellerviana <muellerviana@gmail.com>
2022-05-11 20:31:03 -0300

    142 - Portando Gentelella para o User

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 
 
+#app-assets
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.js users_backoffice.css)
 
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
 
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+#lib-assets
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js img.jpg)
