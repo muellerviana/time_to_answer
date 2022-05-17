f6d44691fb998db3663fbd97d9b3719c16e961f9
muellerviana <muellerviana@gmail.com>
2022-05-17 17:39:33 -0300

    156 - refatorando admins controller

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,20 +1,18 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+before_action :verify_password, only: [:update]
+before_action :set_admin, only: [:edit, :update]
+
   def index
     @admins = Admin.all
   end
 
 
   def edit
-    @admin = Admin.find(params[:id])
   end
 
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-    params[:admin].extract!(:password, :password_confirmation)
-    end
-
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+    
+    @admin = 
   
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso"
@@ -22,4 +20,22 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
       render :edit
     end
   end
+  
+  private
+
+  def params_admin
+    Admin.find(params[:id])
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end 
+
+  def  verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+
 end
