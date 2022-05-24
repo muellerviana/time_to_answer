6e259d68fa84ede8680683f61703ce717bb7b317
muellerviana <muellerviana@gmail.com>
2022-05-24 16:58:25 -0300

    171 - Boa prática para MVC

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,54 +1,58 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
-before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:new, :edit]
     
-    
-def index
-  @questions = Question.all.order(:description).page(params[:page])
-end
-
-def new
-  @question = Question.new
-end
-
-
-def create
-    @question = Question.new(params_question)
-    if @question.save
-    redirect_to admins_backoffice_question_path, notice: "Questão
-    cadastrada com sucesso!"
-    else
-    render :new
+    def index
+    @questions = Question.all.order(:description).page(params[:page])
     end
-end
-    
-def edit
-end
-    
 
-def update
-    if @question.update(params_question)
+    def new
+    @question = Question.new
+    end
+
+
+    def create
+        @question = Question.new(params_question)
+        if @question.save
         redirect_to admins_backoffice_question_path, notice: "Questão
-    atualizada com sucesso!"
-    else
-        render :edit
+        cadastrada com sucesso!"
+        else
+        render :new
+        end
     end
-end
-
-def destroy
-    if @question.destroy
-    redirect_to admins_backoffice_question_path, notice: "Questão excluída com sucesso!"
-    else
-    render :index
+        
+    def edit
     end
-end
+        
 
-private
+    def update
+        if @question.update(params_question)
+            redirect_to admins_backoffice_questions_path, notice: "Questão
+        atualizada com sucesso!"
+        else
+            render :edit
+        end
+    end
 
-def params_question
-    params.require(:question).permit(:description, :subject_id)
-end
+    def destroy
+        if @question.destroy
+        redirect_to admins_backoffice_question_path, notice: "Questão excluída com sucesso!"
+        else
+        render :index
+        end
+    end
 
-def set_question
-    @question = Question.find(params[:id])
-end
+    private
+
+    def params_question
+        params.require(:question).permit(:description, :subject_id)
+    end
+
+    def set_question
+        @question = Question.find(params[:id])
+    end
+
+    def get_subjects
+        @subjects = Subject.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
-  
   namespace :users_backoffice do
     get 'welcome/index'
   end
-
-
- namespace :admins_backoffice do
-    get 'welcome/index'
-    resources :admins
-    resources :subjects 
+  namespace :admins_backoffice do
+    get 'welcome/index' # Dashboard
+    resources :admins   # Administradores
+    resources :subjects # Assuntos/Áreas
+    resources :questions # Perguntas
   end
-
+  
   devise_for :admins
   devise_for :users
-  
-  root 'site/welcome#index'
+ 
+  get 'inicio', to: 'site/welcome#index'
+
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
